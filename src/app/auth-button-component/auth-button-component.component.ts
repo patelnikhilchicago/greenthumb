b6128e1b4320eaa01eb5681759d0240cb9b5ba04
Nikhil Patel <patelnikhil@chicagobooth.edu>
2025-04-05 15:33:27 -0500

    first commit

---
+++ src/app/auth-button-component/auth-button-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
+@Component({
+  selector: 'app-auth-button-component',
+  imports: [],
+  templateUrl: './auth-button-component.component.html',
+  styleUrl: './auth-button-component.component.css'
+})
+
+export class AuthButtonComponentComponent {
+
+}
