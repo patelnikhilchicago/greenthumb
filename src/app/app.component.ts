6ae61251255af50399d964bef356e4382fc05d64
Nikhil Patel <patelnikhil@chicagobooth.edu>
2025-04-05 15:50:55 -0500

    Gemini integrated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { GoogleGenAI } from "@google/genai";
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,18 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'greenthumbs';
+  ai = new GoogleGenAI({ apiKey: "AIzaSyBUiOoBA_vCKxwiqMjdT8VXF5DBMGTqn74" });
+  ngOnInit(): void{
+this.main();
+  }
+
+  async main() {
+    const response = await this.ai.models.generateContent({
+      model: "gemini-2.0-flash",
+      contents: "Explain how AI works in a few words",
+    });
+    console.log(response.text);
+  }
+
+   
 }
--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,6 @@ import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { provideAuth0 } from '@auth0/auth0-angular';
 
+//Gemini API Key: AIzaSyBUiOoBA_vCKxwiqMjdT8VXF5DBMGTqn74
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
