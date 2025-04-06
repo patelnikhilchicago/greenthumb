4df13b9a47cdf69acfa98f3383bcbbc91c36a5f7
Nikhil Patel <patelnikhil@chicagobooth.edu>
2025-04-05 20:23:24 -0500

    Color slider working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild, Input, OnChanges  } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { GoogleGenAI } from "@google/genai";
 import * as fs from "node:fs";
 import { FormsModule } from '@angular/forms';
-import { compileFromFile } from 'json-schema-to-typescript';
 import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
@@ -12,7 +12,20 @@ import { CommonModule } from '@angular/common';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent{
+
+  @Input() sunlight: number = 80;  // percentage (0–100)
+  percent = Math.min(Math.max(this.sunlight, 0), 100); // clamp 0–100
+  progressStyle: string = `conic-gradient(
+    #fbbc04 0% ${this.percent}%,
+    #e0e0e0 ${this.percent}% 100%
+  )`;
+
+  
+   
+
+  
+
   title = 'greenthumbs';
   ai = new GoogleGenAI({ apiKey: "AIzaSyBUiOoBA_vCKxwiqMjdT8VXF5DBMGTqn74" });
   searchInput: any;
@@ -25,7 +38,7 @@ export class AppComponent {
 
 
   async searchBasedonCity(){
-    const prompt = `In ${this.searchInput} what plants can I grow around this time of the year, only provide the names of vegetables, fruits and herbs.  provide your output using one list that is comma separated. DO NOT INCLUDE ANY OTHER DETAIL EXPECT A COMMA SEPARATED LIST`;
+    const prompt = `In ${this.searchInput} what plants can I grow around this time of the year, only provide the names of vegetables, fruits and herbs.  provide your output using one list that is comma separated. DO NOT INCLUDE ANY OTHER DETAIL EXPECT A COMMA SEPARATED LIST. ONLY PROVIDE THE NAME, NOT THE CATEGORY.`;
 
     const response = await this.ai.models.generateContent({
         model: "gemini-2.0-flash",
@@ -39,7 +52,7 @@ export class AppComponent {
     console.log(this.searchResultsArray.forEach(element => {
       console.log(element);
     }));
-    
+
   }
 
   async Search(){
